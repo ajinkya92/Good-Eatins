b61ae23f3d5a321d0a87871d4a9c7fef1bc9b220
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-09-20 03:47:28 +0530

    Models and Structs Created along with adding hardcoded data

---
+++ Good Eatins/Model/FoodCategory.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCategory.swift
+//  Good Eatins
+//
+//  Created by Ajinkya Sonar on 20/09/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+    
+}
---
+++ Good Eatins/Model/Recipe.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe.swift
+//  Good Eatins
+//
+//  Created by Ajinkya Sonar on 20/09/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+    
+}
